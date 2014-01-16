36e5df55b0f371c6f8b8ab6d025bb329b801e9e8
Igor Sverdlov <blizzard@yandex-team.ru>
2014-01-16 19:11:31 +0400

    fix error

--- lib/Dist/Zilla/Plugin/ChangelogFromGit/Debian/Sequential.pm
+++ lib/Dist/Zilla/Plugin/ChangelogFromGit/Debian/Sequential.pm
@@ -74,7 +74,7 @@ sub render_changelog {
 
         $content =
             "$pkg_name ($version) $pkg_distr; urgency=low\n\n"
-          . join("\n\n", @changes || '  * No changes') . "\n\n"
+          . join("\n\n", @changes ? @changes : '  * No changes') . "\n\n"
           . " -- $ENV{'DEBFULLNAME'} <$ENV{'DEBEMAIL'}>  "
           . DateTime::Format::Mail->format_datetime($release->date->clone->set_time_zone('local'))
           . "\n\n$content";
